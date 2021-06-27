20d5c1cf4bd0595714e6ca7d8cb4f2b70765e7e7
Mr.Dokk <jdorjbat052@gmail.com>
2021-06-27 20:17:04 +0800

    Gift zaswar hiisen

--- src/component/GiftSection/index.js
+++ src/component/GiftSection/index.js
@@ -5,10 +5,10 @@ import css from "./style.module.css";
 const GiftSection = (props) => {
   return (
     <div className={css.GiftSection}>
-      <GiftImage image={props.image1} />
-      <GiftImage image={props.image2} />
-      <GiftImage image={props.image3} />
-      <GiftImage image={props.image4} />
+      <GiftImage className={css.section} image={props.image1} />
+      <GiftImage className={css.section} image={props.image2} />
+      <GiftImage className={css.section} image={props.image3} />
+      <GiftImage className={css.section} image={props.image4} />
     </div>
   );
 };
